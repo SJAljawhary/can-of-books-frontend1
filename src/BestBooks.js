1912da366f544f920dd20c050fcbf4f6915edc8e
SJAljawhary <sndjehad@gmail.com>
2021-07-12 23:24:59 +0300

    solving lab12

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,58 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import {Jumbotron , Card} from 'react-bootstrap';
 import './BestBooks.css';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class MyFavoriteBooks extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookData: []
+    }
+  }
+    componentDidMount = async () => {
+
+      const { user } = this.props.auth0;
+      //http://localhost:3003/books?userEmail=sndjehad@gmail.com
+
+      let url = `http://localhost:3003/books?userEmail=${user.email}`
+
+      let responseData = await axios.get(url);
+
+      await this.setState({
+        bookData: responseData.data
+      })
+
+    }
+  
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+    return (
+       <div>
+      {this.state.bookData == null ?
+          <Jumbotron>
+            <h1>My Favorite Books</h1>
+            <p>
+              This is a collection of my favorite books
+            </p>
+          </Jumbotron>
+          :
+
+          this.state.bookData.map(item => {
+            return (
+              <Card>
+                <Card.Body> Name :{item.name}</Card.Body>
+                <Card.Body> Description :{item.description}</Card.Body>
+                <Card.Body> Status : {item.status}</Card.Body>
+              </Card>
+            )
+          })
+
+      }</div>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
